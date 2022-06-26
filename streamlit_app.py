7ad5ff540838ad63043fdcf6f222feec54f6a483
dtirado07 <45781157+dtirado07@users.noreply.github.com>
2022-06-26 17:27:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Blueberry oatmeal')
+streamlit.text('Kale, spinach & Rocket Smoothie')
+streamlit.text('Hard boiled free ranch egg')
